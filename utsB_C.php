0d428dcdb05b2e13a9a669232fb77021a4df9477
zahraayundira <103235945+zzdira@users.noreply.github.com>
2023-11-08 16:31:30 +0700

    utsB_C.php

--- utsB_C.php
+++ utsB_C.php
@@ -3,48 +3,88 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tabel Harga Barang</title>
 </head>
 <body>
-<?php 
-$Dicount = [
-    [
-        "tanggal" => "11 november 2023", 
-        "product" => [
-            "Baju Bayi" => 71000,
-            "Diapers" => 153000,
-            "Bedak" => 15000,
-            "Minyak Telon" => 60000,
-            "baby oil" => 48000
-        ],
-    ]
-];
-?>
-<div style="border: 1px solid #000;padding: 3px; width:fit-content">
 <?php
-foreach ($Dicount as $discountTotal) {
-    $total = $discountTotal["product"]["Baju Bayi"]+$discountTotal["product"]["Diapers"]+$discountTotal["product"]["Bedak"]+$discountTotal["product"]["Minyak Telon"]+$discountTotal["product"]["baby oil"];
-    if ($total >= 150000 ) {
-        $discount=(($total*10)/100);
-        $total_bayar=($total-$discount);
-    }else if ($total >= 200000) {
-        $discount=(($total*20)/100);
-        $total_bayar=($total-$discount);
-    }
 
-    echo "<p>"."Tanggal Produksi    :".$discountTotal["tanggal"]."</p>";
-    echo "<p>"."Product             :"."</p>";
-    echo "<p>"."Baju Bayi(2X35.500) :"."Rp.".$discountTotal["product"]["Baju Bayi"]."</p>";
-    echo "<p>"."Diapers(3X51.000)     :"."Rp.".$discountTotal["product"]["Diapers"]."</p>";
-    echo "<p>"."Bedak(1X15.000)      :"."Rp.".$discountTotal["product"]["Bedak"]."</p>";
-    echo "<p>"."Minyak Telon(2X30.000) :"."Rp.".$discountTotal["product"]["Minyak Telon"]."</p>";
-    echo "<p>"."baby oil(3x16.000)   :"."Rp.".$discountTotal["product"]["baby oil"]."</p>";
-    echo "<p>"."Total               :"."Rp.".$total."</p>";
-    echo "<p>"."Discount            :"."Rp.".$discount."</p>";
-    echo "<p>"."Total Pembayaran    :"."Rp.".$total_bayar."</p>";
-}
+$TableHargaBarang = array(
+    array(
+        "ID" => 1,
+        "Product" => "Minyak telon",
+        "Stok" => 20,
+        "Harga" => 30000,
+        // "Jumlah" => Stok * Harga,
+    ),
+    array(
+        "ID" => 2,
+        "Product" => "Diapers",
+        "Stok" => 15,
+        "Harga" => 51000,
+        // "Jumlah" => Stok * Harga,
+    ),
+    array(
+        "ID" => 3,
+        "Product" => "Baby Oil",
+        "Stok" => 10,
+        "Harga" => 16000,
+        // "Jumlah" => Stok * Harga,
+    ),
+    array(
+        "ID" => 4,
+        "Product" => "Shampo Baby",
+        "Stok" => 18,
+        "Harga" => 20000,
+        // "Jumlah" => Stok * Harga,
+    ),
+    array(
+        "ID" => 5,
+        "Product" => "Bedak",
+        "Stok" => 15,
+        "Harga" => 15000,
+        // "Jumlah" => Stok * Harga,
+    ),
+    array(
+        "ID" => 6,
+        "Product" => "Baju Bayi",
+        "Stok" => 20,
+        "Harga" => 35500,
+        // "Jumlah" => Stok * Harga,
+    ),
+    array(
+        "ID" => 7,
+        "Product" => "Jumper",
+        "Stok" => 25,
+        "Harga" => 50000,
+        // "Jumlah" => Stok * Harga,
+    ),
+);
+
+// Mencetak tabel
 ?>
-</div>
-</div>
+
+<h1 style="text-align: center;">Table Harga Barang</h1>
+<table border="1" cellpadding="3" cellspacing="0" style=" margin: 0 auto;">
+    <tr>
+        <th>ID</th>
+        <th>Produk</th>
+        <th>Stok</th>
+        <th>Harga</th>
+        <th>Jumlah</th>
+    </tr>
+
+    <?php 
+    foreach ($TableHargaBarang as $Barang) {
+        echo "<tr>";
+        echo "<td>".$Barang["ID"]."</td>";
+        echo "<td>" . $Barang["Product"] . "</td>";
+        echo "<td>" . $Barang["Stok"] . "</td>";
+        echo "<td>" . $Barang["Harga"] . "</td>";
+        echo "<td>" . $Barang["Stok"] * $Barang["Harga"] . "</td>";
+        echo "</tr>";
+    };
+    
+    ?>
+</table>
 </body>
 </html>
